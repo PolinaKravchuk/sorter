28b2a078b4213931327975f4dec385b8ba4c309e
Polina Kravchuk <34372082+PolinaKravchuk@users.noreply.github.com>
2018-02-23 22:52:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
-class Sorter {
-  constructor() {
-    // your implementation
-  }
-
-  add(element) {
-    // your implementation
-  }
-
-  at(index) {
-    // your implementation
-  }
-
-  get length() {
-    // your implementation
-  }
-
-  toArray() {
-    // your implementation
-  }
 
   sort(indices) {
-    // your implementation
+    //var arr = indices.split(',');
+    for (var i =0; i < indices.length - 1; i++)
+      for (var j = 1; j < indices.length; j++){
+          if (indices[i] > indices[j]){
+            var t = indices[i];
+            indices[i] = indices[j];
+            indices[j] = t;
+          }
+      }
+    for (var i =0; i < indices.length - 1; i++)
+      for (var j = 1; j < indices.length; j++){
+        if (this.mass[indices[i]] > this.mass[indices[j]]) {
+          t = this.mass[indices[i]];
+          this.mass[indices[i]] = this.mass[indices[j]];
+          this.mass[indices[j]] = t;
+        }
+      }
+     
+
+    return  this.mass;
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    return a - b;
   }
 }
 
